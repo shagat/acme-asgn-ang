62a2292b34da718ec8e8898b69911444e7cabf83
Dave <88671265+shagat@users.noreply.github.com>
2022-08-26 02:14:45 +0530

    added media for responsive mobile viewing

--- acme/src/app/layout/content/product/product.component.ts
+++ acme/src/app/layout/content/product/product.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  link =
+    'https://www.hexnode.com/resources/video/?resourcepost=MTQ0NA==&datacount=1';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
