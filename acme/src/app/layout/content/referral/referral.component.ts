98009edca9dd142edf5f8d9bff12f7c1665bdb52
Dave <88671265+shagat@users.noreply.github.com>
2022-08-25 23:37:05 +0530

    added table

--- acme/src/app/layout/content/referral/referral.component.ts
+++ acme/src/app/layout/content/referral/referral.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-referral',
   templateUrl: './referral.component.html',
-  styleUrls: ['./referral.component.css']
+  styleUrls: ['./referral.component.css'],
 })
 export class ReferralComponent implements OnInit {
+  data = [
+    {
+      location: 'google.com',
+      views: 3746,
+      sales: 752,
+      conversion: 43,
+      total: 19291,
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
