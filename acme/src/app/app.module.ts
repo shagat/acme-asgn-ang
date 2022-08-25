a52263e21a762479dafbc2c710d26e0149b20527
Dave <88671265+shagat@users.noreply.github.com>
2022-08-25 13:17:50 +0530

    content grid styling

--- acme/src/app/app.module.ts
+++ acme/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SideNavComponent } from './layout/side-nav/side-nav.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { MainComponent } from './layout/main/main.component';
+import { ContentComponent } from './layout/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideNavComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
