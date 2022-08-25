007cdbeb7dce13459c5d7bf084c9ea936f911093
Dave <88671265+shagat@users.noreply.github.com>
2022-08-26 00:46:11 +0530

    added referral section

--- acme/src/app/layout/content/referral/referral.component.ts
+++ acme/src/app/layout/content/referral/referral.component.ts
@@ -14,6 +14,41 @@ export class ReferralComponent implements OnInit {
       conversion: 43,
       total: 19291,
     },
+    {
+      location: 'facebook.com',
+      views: 8246,
+      sales: 642,
+      conversion: 63,
+      total: 15291,
+    },
+    {
+      location: 'twitter.com',
+      views: 8846,
+      sales: 652,
+      conversion: 23,
+      total: 19291,
+    },
+    {
+      location: 'Direct, email, IM',
+      views: 1146,
+      sales: 642,
+      conversion: 26,
+      total: 13291,
+    },
+    {
+      location: 'linkdin.com',
+      views: 2746,
+      sales: 552,
+      conversion: 20,
+      total: 12291,
+    },
+    {
+      location: 'instagram.com',
+      views: 2746,
+      sales: 552,
+      conversion: 23,
+      total: 12291,
+    },
   ];
   constructor() {}
 
