bd8f66e4ca7457f3e31f85de4f9937e524249c69
Dave <88671265+shagat@users.noreply.github.com>
2022-08-26 15:48:04 +0530

    changes charts

--- acme/src/app/layout/content/statistics/statistics.component.ts
+++ acme/src/app/layout/content/statistics/statistics.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { Chart, registerables } from 'chartjs
 import { Chart, registerables } from '../../../../../node_modules/chart.js';
 @Component({
   selector: 'app-statistics',
